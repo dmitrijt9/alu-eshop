99636f6542656280fef869792adf3dee4453a265
Dmitrij Tkacenko <dimot9@gmail.com>
2022-01-09 21:01:48 +0100

    Add homepage wip

--- app/FrontModule/Presenters/HomepagePresenter.php
+++ app/FrontModule/Presenters/HomepagePresenter.php
@@ -2,8 +2,22 @@
 
 namespace App\FrontModule\Presenters;
 
+use App\Model\Facades\ProductsFacade;
+
 class HomepagePresenter extends BasePresenter{
 
-  //TODO
+    /** @var ProductsFacade $productsFacade */
+    private $productsFacade;
+
+    /**
+     * Akce pro vykreslení seznamu produktů
+     */
+    public function renderDefault():void {
+        $this->template->products=$this->productsFacade->findProducts(['order'=>'title']);
+    }
+
+    public function injectProductsFacade(ProductsFacade $productsFacade){
+        $this->productsFacade=$productsFacade;
+    }
 
 }
